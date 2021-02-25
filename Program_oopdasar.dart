21258a5ab03c1c0004ce52bbc6472a3be9f8801c
Muit Sunjaya <79581302+Muit-Sunjaya@users.noreply.github.com>
2021-02-25 15:20:15 +0000

    Update Program_oopdasar.dart

--- Program_oopdasar.dart
+++ Program_oopdasar.dart
@@ -1,1 +1,30 @@
+class Person{
+  String _name;
+  var _address;
 
+  String getName(){
+    return this._name;
+  }
+
+  void setName(String name){
+    this._name = name;
+  }
+
+  String getAddress(){
+    return this._address;
+  }
+
+  void setAddress(String address){
+    this._address = address;
+  }
+}
+
+// fungsi main
+main(){
+  var muit = new Person();
+  Muit.setName("muit");
+  dian.setAddress("medan");
+
+  print("Nama: ${muit.getName()}");
+  print("Alamat: ${muit.getAddress()}");
+}
