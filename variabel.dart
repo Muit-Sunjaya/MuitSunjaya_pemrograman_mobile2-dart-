6388be87a7e7f22fb862c6df1227447eaaa8b764
Muit Sunjaya <79581302+Muit-Sunjaya@users.noreply.github.com>
2021-02-25 15:10:16 +0000

    Update variabel.dart

--- variabel.dart
+++ variabel.dart
@@ -1,1 +1,10 @@
-
+void main() {
+   String nama = "Muit";
+     int umur = 22;
+       double tinggi = 168.43;
+         bool isMenikah = false;
+   var alamat = "medan, Indonesia";
+   print("Nama saya $nama. Umur $umur tahun. Tinggi sekitar $tinggi cm.");
+     print("Menikah: $isMenikah");
+       print("Alamat : $alamat");
+       }
